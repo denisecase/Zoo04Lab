f2342c022712df2fa47c0358faf5196d03985fba
VIJAY KUMAR CHUNCHU <89609317+Vkvarmach@users.noreply.github.com>
2021-10-19 10:04:57 -0500

    Update MightyLion.java
    updating Test case

--- src/main/java/edu/nwmissouri/zoo04lab/MightyLion.java
+++ src/main/java/edu/nwmissouri/zoo04lab/MightyLion.java
@@ -23,6 +23,32 @@ public class MightyLion extends Animal {
     public void speak() {
          System.out.printf("I am %s Lion. I will roar.\n", this.name);
     }
-    
+    public void profess() {
+        double a = 2.5;
+        int b = 2;
+        double c = getMightyLionkAddition(a, b);
+        System.out.printf("The MightyLionAddition is  %4.2f plus %d is %4.2f \n", a, b, c);
+    }
+
+   /*
+     * @param valueOne double input
+     * @param valueTwo int input
+     * @return double sum
+     */
+    public double getMightyLionAddition(double valueOne, int valueTwo) {
+        return valueOne + valueTwo;
+    }
+
+    public static void main(String[] args) {
+        var a = new Aardvark("Tester");
+        a.speak();
+        a.move();
+        a.profess();
+    }
+
+    private double getMightyLionkAddition(double a, int b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
    
 }
